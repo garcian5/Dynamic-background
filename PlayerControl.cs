ccb51897e0535d2eceb272dfccc1f0ff3a5fb530
unbeknownst6k9 <46736851+unbeknownst6k9@users.noreply.github.com>
2019-05-30 11:56:28 -0600

    Add files via upload
    Put those scripts under a gameObject named player and set the max and min value to non-zero and drag the "player" into the box where it says player then it should be working

---
+++ PlayerControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public int Health = 1000;
+    public float xMax = 0, xMin = 0;
+    //get a reference for gameobject player
+    public GameObject player;
+    GenerateMove geneMove;
+
+    public void MovePlayer()
+    {
+        //take a reference to the player position
+        Vector3 movement = transform.position;
+        geneMove = GetComponent<GenerateMove>();
+        if (Input.GetMouseButton(0))
+        {
+            movement.x += geneMove.getDirection();
+            transform.position = new Vector3(
+                Mathf.Clamp(movement.x,xMin,xMax),
+                transform.position.y,
+                transform.position.z
+                );
+        }
+    }
+
+    //return the player position for random generation in 
+    //generatemove script
+    public Vector3 getPlayerPosition()
+    {
+        return transform.position;
+    }
+
+    public float getMaxMap()
+    {
+        return xMax;
+    }
+
+    public float getMinMap()
+    {
+        return xMin;
+    }
+
+    //player functionality: damage
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Block" || other.tag == "block")
+        {
+            Health -= 10;
+        }
+        else if (other.tag == "Recover" || other.tag == "recover")
+        {
+            Health += 10;
+        }
+    }
+    
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+}
